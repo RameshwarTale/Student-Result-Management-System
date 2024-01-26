b2120e89df00619a3f4a4cbaae484c9c0c9fcdcf
Rameshwar Tale <ramgtale21@gmail.com>
2024-01-26 13:43:29 +0530

    Software for displaying student results

---
+++ src/student_result_management_protal1/STUDENT_RESULT_MANAGEMENT_PROTAL1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package student_result_management_protal1;
+
+/**
+ *
+ * @author Rameshwar
+ */
+public class STUDENT_RESULT_MANAGEMENT_PROTAL1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+
+    }
+    
+}
